d064d657d2e03ef928abad1e7d54e1e22987a87f
pg001001 <152976800+pg001001@users.noreply.github.com>
2024-09-04 12:28:46 +0530

    Add files via upload

--- information.sh
+++ information.sh
@@ -30,7 +30,8 @@ scan_information() {
 
     # sensitive information in downloaded JavaScript files
     echo "Searching for sensitive information in JavaScript files..."
-    grep -r --color=always -i -E "aws_access_key|aws_secret_key|api key|passwd|pwd|heroku|slack|firebase" "${base_dir}/js_files/" | tee "${base_dir}/information/sensitive_info.txt"
+    grep -r --color=always -i -E "aws_access_key|aws_secret_key|api_key" "${base_dir}/js_files/" | tee "${base_dir}/information/sensitive_info.txt"
+    # grep -r --color=always -i -E "aws_access_key|aws_secret_key|api key|passwd|pwd|heroku|slack|firebase" "${base_dir}/js_files/" | tee "${base_dir}/information/sensitive_info.txt"
     # grep -r --color=always -i -E "access_key|access_token|admin_pass|admin_user|algolia_admin_key|algolia_api_key|alias_pass|alicloud_access_key|amazon_secret_access_key|amazonaws|ansible_vault_password|aos_key|api_key_sid|api.googlemaps AIza|apidocs|appkeysecret|application_key|appsecret|appspot|auth_token|authorizationToken|authsecret|aws_access|aws_access_key_id|aws_bucket|aws_key|aws_secret|aws_secret_key|aws_token|AWSSecretKey|b2_app_key|bashrc password|bintray_apikey|bintray_gpg_password|bintray_key|bintraykey|bluemix_api_key|bluemix_pass|browserstack_access_key|bucket_password|bucketeer_aws_access_key_id|bucketeer_aws_secret_access_key|built_branch_deploy_key|bx_password|cache_driver|cache_s3_secret_key|cattle_access_key|cattle_secret_key|certificate_password|ci_deploy_password|client_secret|client_zpk_secret_key|clojars_password|cloud_api_key|cloud_watch_aws_access_key|cloudant_password|cloudflare_api_key|cloudflare_auth_key|cloudinary_api_secret|cloudinary_name|codecov_token|conn.login|connectionstring|consumer_key|consumer_secret|cypress_record_key|database_password|database_schema_test|datadog_api_key|datadog_app_key|db_password|db_server|db_username|dbpasswd|dbpassword|dbuser|deploy_password|digitalocean_ssh_key_body|digitalocean_ssh_key_ids|docker_hub_password|docker_key|docker_pass|docker_passwd|docker_password|dockerhub_password|dockerhubpassword|dot-files|dotfiles|droplet_travis_password|dynamoaccesskeyid|dynamosecretaccesskey|elastica_host|elastica_port|elasticsearch_password|encryption_key|encryption_password|heroku_api_key|sonatype_password|awssecretkey" "${base_dir}/js_files/" >> "${base_dir}//information/sensitive_info.txt"
     
     # endpoints
@@ -53,7 +54,10 @@ scan_information() {
     grep -r --color=always -i -E "gmail | yahoo | hotmail " "${base_dir}/allurls.txt" >> "${base_dir}/information/common_emails.txt"
 
     # billngs
-    grep -r --color=always -i -E "invoce | price | billing | payment" "${base_dir}/allurls.txt" >> "${base_dir}/information/billings.txt"
+    grep -r --color=always -i -E "invoice | price | billing | payment" "${base_dir}/allurls.txt" >> "${base_dir}/information/billings.txt"
+    
+    # credentials
+    grep -r --color=always -i -E ":" "${base_dir}/allurls.txt" >> "${base_dir}/information/credentials"
     
     rm -r "${base_dir}/js_files/"
 
